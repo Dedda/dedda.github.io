e3f2cd13d14cf91defc2747429d6de5d5156780e
Stephan Göppentin <dedda102@gmail.com>
2015-04-14 18:25:24 +0200

    added debug funtionality

--- generate_projects.py
+++ generate_projects.py
@@ -4,12 +4,17 @@ __author__ = 'dedda'
 
 import json
 import jinja2
+import sys
 
 TEMPLATES_FOLDER = 'template/'
 
 JSON_FILE = 'projects.json'
 TEMPLATE_FILE = 'projects.html'
 RENDERED_FILE = 'projects.html'
+
+debug = 'debug' in sys.argv
+
+print('debug: ' + str(debug))
 
 templateLoader = jinja2.FileSystemLoader(searchpath=TEMPLATES_FOLDER)
 templateEnv = jinja2.Environment(loader=templateLoader)
@@ -18,7 +23,7 @@ print('generating projects page...')
 
 json_data = open(JSON_FILE).read()
 data = json.loads(json_data)
-template_data = {}
+template_data = {'debug' : debug}
 template_data['projects'] = {}
 for (name, project) in data.items():
     print("found project '" + name + "'")

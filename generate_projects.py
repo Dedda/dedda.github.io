2c1adc7475fc7e2ae427120dea2c00a28e098ec0
Stephan Göppentin <dedda102@gmail.com>
2015-04-14 18:11:44 +0200

    fixed projects and made script to projects generator only

---
+++ generate_projects.py
@@ -0,0 +1,32 @@
+from pprint import pprint
+
+__author__ = 'dedda'
+
+import json
+import jinja2
+
+TEMPLATES_FOLDER = 'template/'
+
+JSON_FILE = 'projects.json'
+TEMPLATE_FILE = 'projects.html'
+RENDERED_FILE = 'projects.html'
+
+templateLoader = jinja2.FileSystemLoader(searchpath=TEMPLATES_FOLDER)
+templateEnv = jinja2.Environment(loader=templateLoader)
+
+print('generating projects page...')
+
+json_data = open(JSON_FILE).read()
+data = json.loads(json_data)
+template_data = {}
+template_data['projects'] = {}
+for (name, project) in data.items():
+    print("found project '" + name + "'")
+    project_data = project
+    project_data['name'] = name
+    template_data['projects'][name] = project_data
+##pprint(projects_template_data)
+template = templateEnv.get_template(TEMPLATE_FILE)
+rendered = open(RENDERED_FILE, 'w')
+rendered.write(template.render(template_data))
+print('projcets page generated!')

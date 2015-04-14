4d5387f7d54d83ab14d143c53b73122c2983415f
Stephan Göppentin <dedda102@gmail.com>
2015-04-14 22:24:40 +0200

    removed comments and unused imports

--- generate.py
+++ generate.py
@@ -1,7 +1,6 @@
 __author__ = 'dedda'
 
 import sys
-import os
 import generate_index
 import generate_projects
 
@@ -11,12 +10,5 @@ print('generating...')
 
 generate_index.generate(debug)
 generate_projects.generate(debug)
-##if debug:
-    ##os.system('python generate_index.py debug')
-    ##os.system('python generate_projects.py debug')
-
-##else:
-    ##os.system('python generate_index.py')
-    ##os.system('python generate_projects.py')
 
 print('generated!')

3b4693a8d45b014cedea7ea9d1285398404a26f3
Stephan Göppentin <dedda102@gmail.com>
2015-04-15 23:18:59 +0200

    used in instead of has_key

--- generate_projects.py
+++ generate_projects.py
@@ -21,7 +21,7 @@ def generate(debug):
         print("found project '" + name + "'")
         project_data = project
         project_data['name'] = name
-        if project.has_key('desc'):
+        if 'desc' in project:
             project_data['desc'] = "".join(project['desc'])
         template_data['projects'][name] = project_data
     template = templateEnv.get_template(TEMPLATE_FILE)
